ae258c03fdf2a26c76dbbc6a73bc5be73f29ac78
alexreyes765 <144854239+alexreyes765@users.noreply.github.com>
2025-11-20 16:21:14 -0600

    elimine directivas sin usar

--- app/src/main/java/com/itsrfid/cursokotlin/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/itsrfid/cursokotlin/firstapp/FirstAppActivity.kt
@@ -2,14 +2,9 @@ package com.itsrfid.cursokotlin.firstapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.itsrfid.cursokotlin.R
 
 class FirstAppActivity : AppCompatActivity() {
